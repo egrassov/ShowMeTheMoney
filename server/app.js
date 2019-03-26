71e8800038e7b93d0fa74f93630e4c68401827c4
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-03-26 17:12:43 +0100

    renaming

--- server/app.js
+++ server/app.js
@@ -3,15 +3,16 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
+
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const cors         = require("cors");
+
 
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -24,30 +25,27 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// Middleware Setup
+
+
+const whiteList = ["http://localhost:3000"]
+
+const corsOptions = {
+  origin: (origin, cb) => {
+    const originIsWhitelisted = whiteList.includes(origin);
+    cb(null, originIsWhitelisted)
+  }
+}
+
+app.use(cors(corsOptions));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
