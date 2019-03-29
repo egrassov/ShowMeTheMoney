a4ea6c7e53fe4e6b24e6753270ee308858b2238c
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-03-29 13:51:51 +0100

    first object picking working properly

--- client/src/components/ThreeScene.js
+++ client/src/components/ThreeScene.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import * as THREE from 'three'
 var OrbitControls = require('three-orbit-controls')(THREE)
-var Stats = require('three-stats')(THREE)
+
+
 
 class ThreeScene extends Component{
   componentDidMount(){
     const width = this.mount.clientWidth
     const height = this.mount.clientHeight
+    console.log(width,height)
     //ADD SCENE
     this.scene = new THREE.Scene()
     //ADD CAMERA
@@ -16,8 +18,9 @@ class ThreeScene extends Component{
       0.1,
       1000
     )
-    this.camera.position.z = 18
-    this.camera.position.y = 4
+    this.camera.position.y = 10
+    this.camera.position.z = 16
+    this.camera.lookAt(this.scene.position);
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
     this.renderer.setClearColor( 0xffffff,0);
@@ -33,25 +36,16 @@ class ThreeScene extends Component{
 
     //ADD CUBE
     const geometry = new THREE.BoxGeometry(3, 3, 3)
-    const geometry3 = new THREE.BoxGeometry(14, 14, 14)
-    const material = new THREE.MeshNormalMaterial({transparent:true})
-    material.opacity = 0.1
-    this.cube2 = new THREE.Mesh(geometry, material)
+    const material = new THREE.MeshBasicMaterial({color:'#4286f4' ,transparent:true})
+    material.opacity = 0.4
     this.cube = new THREE.Mesh(geometry, material)
-    this.cube3 = new THREE.Mesh(geometry3, material)
-    this.cube.scale.x = 3
-    this.cube.scale.y = 3
-    this.cube.scale.z = 3
-    this.scene.add(this.cube2,this.cube,this.cube3)
 
-    this.projector = {
-        x: 0,
-        y: 0
-      }
-    this.mouse = {
-        x: 0,
-        y: 0
-      }
+    this.cube.name = "popino"
+
+    this.scene.add(this.cube)
+
+    this.raycaster = new THREE.Raycaster(); // create once
+    this.mouse = new THREE.Vector2(); // create once  
     this.INTERSECTED = undefined
 
     this.projector = new THREE.Projector();
@@ -80,70 +74,94 @@ class ThreeScene extends Component{
         // event.preventDefault();
       
         // update the mouse variable
-        console.log(this.mouse)
-        this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+        this.mouse.x = ( event.clientX / this.renderer.domElement.clientWidth ) * 2 - 1;
+        this.mouse.y = - ( event.clientY / this.renderer.domElement.clientHeight ) * 2 + 1;
+        this.update()
       }
 
     animate = () => {
         this.cube.rotation.y += 0.01
-        this.cube3.rotation.y -= 0.01
-        this.cube2.rotation.y -= 0.01
         this.renderScene()
         this.frameId = window.requestAnimationFrame(this.animate)
     }
 
     update = () => {
+
+
+
+        this.raycaster.setFromCamera( this.mouse, this.camera );
+
+        this.intersects = this.raycaster.intersectObjects( this.scene.children );
+        if(this.intersects.length>0){
+          console.log(this.intersects[0].object.name)
+        }
         // find intersections
       
         // create a Ray with origin at the mouse position
         //   and direction into the scene (camera direction)
-        this.vector = new THREE.Vector3(this.mouse.x, this.mouse.y, 1);
-        this.vector.unproject(this.camera);
-        this.ray = new THREE.Raycaster(this.camera.position, this.vector.sub(this.camera.position).normalize());
+        // this.vector = new THREE.Vector3(this.mouse.x, this.mouse.y, 1);
+        // console.log(this.vector)
+        // this.vector.unproject(this.camera);
+        // console.log("holi")
+        // this.ray = new THREE.Raycaster(this.camera.position, this.vector.sub(this.camera.position).normalize());
       
-        // create an array containing all objects in the scene with which the ray intersects
-        this.intersects = this.ray.intersectObjects(this.scene.children);
+        // // create an array containing all objects in the scene with which the ray intersects
+        // this.intersects = this.ray.intersectObjects(this.scene.children);
+        // if(this.intersects.length>0){
+        //     console.log(this.intersects[0].object)
+        // }
+        
       
-        // INTERSECTED = the object in the scene currently closest to the camera 
-        //		and intersected by the Ray projected from the mouse position 	
-      
-        // if there is one (or more) intersections
-        if (this.intersects.length > 0) {
-          // if the closest object intersected is not the currently stored intersection object
-          if (this.intersects[0].object != this.INTERSECTED) {
-            // restore previous intersection object (if it exists) to its original color
-            if (this.INTERSECTED)
-              this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex);
-            // store reference to closest object as current intersection object
-            this.INTERSECTED = this.intersects[0].object;
-            // store color of closest object (for later restoration)
-            this.INTERSECTED.currentHex = this.INTERSECTED.material.color.getHex();
-            // set a new color for closest object
-            this.INTERSECTED.material.color.setHex(0xffff00);
-          }
-        } else // there are no intersections
-        {
+      // INTERSECTED = the object in the scene currently closest to the camera 
+      // 		and intersected by the Ray projected from the mouse position 	
+    
+      // if there is one (or more) intersections
+      if (this.intersects.length > 0) {
+        // if the closest object intersected is not the currently stored intersection object
+        if (this.intersects[0].object !== this.INTERSECTED) {
           // restore previous intersection object (if it exists) to its original color
-          if (this.INTERSECTED)
-            this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex);
-          // remove previous intersection object reference
-          //     by setting current intersection object to "nothing"
-          this.INTERSECTED = null;
+          if (this.INTERSECTED) {
+            this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex)
+            console.log(this.INTERSECTED.position.y)
+            this.INTERSECTED.position.y -= 0.4
+          }
+            
+          // store reference to closest object as current intersection object
+          this.INTERSECTED = this.intersects[0].object;
+          // store color of closest object (for later restoration)
+          this.INTERSECTED.currentHex = this.INTERSECTED.material.color.getHex()
+          this.INTERSECTED.currentpositiony = this.INTERSECTED.position.y
+          // set a new color for closest object
+          this.INTERSECTED.material.color.setHex(0xffff00)
+          this.INTERSECTED.position.y += 0.4
         }
+      } else // there are no intersections
+      {
+        // restore previous intersection object (if it exists) to its original color
+        if (this.INTERSECTED) {
+          this.INTERSECTED.material.color.setHex(this.INTERSECTED.currentHex)
+          console.log(this.INTERSECTED.position.y)
+          this.INTERSECTED.position.y = 0
+        }
+          
+        // remove previous intersection object reference
+        //     by setting current intersection object to "nothing"
+        this.INTERSECTED = null
+      }
       
-        this.controls.update();
-        this.stats.update();
+        // this.controls.update();
+        // // this.stats.update();
       }
 
 
     renderScene = () => {
-    this.renderer.render(this.scene, this.camera)
+        this.renderer.render(this.scene, this.camera)
     }
     render(){
         return(
         <div
-            style={{ margin: '0 auto' ,paddingTop: '200px' ,width: '50%', height: '50%' }}
+            style={{ margin: '0 auto' ,width: '100vw', height: '100vh' }}
             ref={(mount) => { this.mount = mount }}
             onMouseMove={this.onDocumentMouseMove}
         />
