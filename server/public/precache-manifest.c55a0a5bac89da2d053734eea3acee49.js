a01a4d2f86a6636ed672bbd33a99369a4acde67d
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-04-03 18:58:23 +0200

    ssss

---
+++ server/public/precache-manifest.c55a0a5bac89da2d053734eea3acee49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b23256b309a683db29fdebb7aa4ab92",
+    "url": "/static/media/right-arrow.1b23256b.svg"
+  },
+  {
+    "revision": "6e60bb23af6d7f86d3b120490e9e0be7",
+    "url": "/static/media/menu.6e60bb23.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38b3816f81ca14680ba4",
+    "url": "/static/js/main.3956486e.chunk.js"
+  },
+  {
+    "revision": "41ac8595b86fa75d67c3",
+    "url": "/static/js/2.27e7ca9e.chunk.js"
+  },
+  {
+    "revision": "38b3816f81ca14680ba4",
+    "url": "/static/css/main.a2b5d1ad.chunk.css"
+  },
+  {
+    "revision": "7559415346a8614c88fdeec40496077a",
+    "url": "/index.html"
+  }
+];
